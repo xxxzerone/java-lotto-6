67f67e6295b154125aa58bca442350fac34cc9b9
xxxzerone <enpunk90@gmail.com>
2024-06-12 12:59:31 +0900

    Feat: ê¾수량만큼 로또 발행

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -1,5 +1,10 @@
 package lotto.domain;
 
+import lotto.util.Generate;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Store {
 
     private static final int DEFAULT_UNIT_AMOUNT = 1000;
@@ -21,7 +26,19 @@ public class Store {
         return amount % DEFAULT_UNIT_AMOUNT != 0;
     }
 
+    public List<Lotto> publishLotto() {
+        return IntStream.range(0, getLottoCount())
+                .mapToObj(i -> new Lotto(getSortedNumbers()))
+                .toList();
+    }
+
     private int getLottoCount() {
         return amount / DEFAULT_UNIT_AMOUNT;
+    }
+
+    private List<Integer> getSortedNumbers() {
+        List<Integer> numbers = Generate.getNumbers();
+        numbers.sort(null);
+        return numbers;
     }
 }
