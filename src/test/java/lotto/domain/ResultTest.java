e99b3bcdcd6e2cb7ff7f163fd32449abcef17db6
xxxzerone <enpunk90@gmail.com>
2024-06-12 11:36:34 +0900

    Test: 발행한 로또 결과

---
+++ src/test/java/lotto/domain/ResultTest.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultTest {
+
+    private Result result;
+
+    @BeforeEach
+    void setUp() {
+        result = new Result(new Lotto(List.of(7, 17, 18, 24, 30, 33)), 42);
+    }
+
+    @ParameterizedTest(name = "[{index}] {1}")
+    @MethodSource("lottoNumbers")
+    @DisplayName("당첨 내역 확인")
+    void winning(Lotto lotto, WinningPolicy winningPolicy) {
+        //given
+
+        //when
+        WinningPolicy winning = result.winning(lotto);
+
+        //then
+        assertThat(winning).isEqualTo(winningPolicy);
+    }
+
+    private static Stream<Arguments> lottoNumbers() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(7, 17, 18, 24, 30, 33)), WinningPolicy.FIRST),
+                Arguments.of(new Lotto(List.of(7, 17, 18, 24, 30, 42)), WinningPolicy.SECOND),
+                Arguments.of(new Lotto(List.of(7, 17, 20, 24, 30, 33)), WinningPolicy.THIRD),
+                Arguments.of(new Lotto(List.of(2, 17, 18, 20, 28, 33)), WinningPolicy.FIFTH)
+        );
+    }
+}
